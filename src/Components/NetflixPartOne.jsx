685ea42d91acdb7642141c345b808d376986301e
Poornima Murali <poornimamuralip@gmal.com>
2022-12-16 11:56:07 +0530

    netflix page css changed

--- src/Components/NetflixPartOne.jsx
+++ src/Components/NetflixPartOne.jsx
@@ -11,15 +11,19 @@ function NetflixPartOne() {
         <div className='mid-session' >
             <input onChange={(e) => { setEmail(e.target.value) }} type="email" name='email' className='input1' placeholder='Enter your email' /> <br />
             <input onChange={(e) => { setPassword(e.target.value) }} type="password" name='password' className='input2' placeholder='Enter your strong password' />
-            <p className='para' >New to Netflix? <a href="#">Sign up now</a></p>
             {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvw-G8FXjZjHJwjEmFxaShQTI9IXtiKaRLOAdYc7Sf&s" className='netflix' width={100} alt="" /> */}
             <div>
-
                 <div className='button-container' >
                     <button onClick={clickSign} className='btn' >SIGNUP</button>
                 </div>
+                <div className='line-paras' >
+
+                    <p>Rember me</p>
+                    <p><a href="#" className='help' >Need help?</a></p>
+                </div>
                 <div className='paras'>
-                    <p className='para' >This page is protected by Google reCAPTCHA <br /> to ensure you're not a bot. <a href="#"> Learn more.</a></p>
+                    <p className='para' >New to Netflix? <a href="#" className='link-sign' >Sign up now</a></p>
+                    <p className='para2' >This page is protected by Google reCAPTCHA <br /> to ensure you're not a bot. <a href="#"> Learn more.</a></p>
                 </div>
             </div>
         </div>
